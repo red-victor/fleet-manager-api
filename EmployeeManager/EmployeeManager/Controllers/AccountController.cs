31b968fe1ccb325cfc11ac014662306ed9bdc6c7
red-victor <77648244+red-victor@users.noreply.github.com>
2021-11-16 16:39:18 +0200

    Create Account Controller

---
+++ EmployeeManager/EmployeeManager/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using EmployeeManager.DTOs;
+using EmployeeManager.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<ApplicationUser>> Login(CredentialDto credentialDto)
+        {
+            var user = await _userManager.FindByEmailAsync(credentialDto.Email);
+
+            if (user == null || !await _userManager.CheckPasswordAsync(user, credentialDto.Password))
+            {
+                return Unauthorized();
+            }
+
+            return user;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(CredentialDto credentialDto)
+        {
+            var user = new ApplicationUser { UserName = credentialDto.Email, Email = credentialDto.Email };
+            var result = await _userManager.CreateAsync(user, credentialDto.Password);
+
+            if (!result.Succeeded)
+            {
+                return ValidationProblem();
+            }
+
+            return Ok();
+        }
+    }
+}
