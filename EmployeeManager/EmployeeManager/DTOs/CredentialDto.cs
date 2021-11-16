d515ceaba2dc627c3b2f2eacb651b09b6c023d47
red-victor <77648244+red-victor@users.noreply.github.com>
2021-11-16 16:39:07 +0200

    Create Credention DTO

---
+++ EmployeeManager/EmployeeManager/DTOs/CredentialDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.DTOs
+{
+    public class CredentialDto
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
