395c6ff5344ab65d349f0b25ccde1833821afc5a
red-victor <77648244+red-victor@users.noreply.github.com>
2021-11-16 14:49:12 +0200

    Add Car Service

---
+++ EmployeeManager/EmployeeManager/Services/CarService.cs
@@ -0,0 +1,49 @@
+﻿using EmployeeManager.Data;
+using EmployeeManager.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Services
+{
+    public class CarService : ICarService
+    {
+        //private readonly ICarDao _carDao;
+        private readonly ApplicationDbContext _db;
+
+        public CarService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Add(Car car)
+        {
+            _db.Cars.Add(car);
+            _db.SaveChanges();
+        }
+
+        public Car Get(int id)
+        {
+            return _db.Cars.Find(id);
+        }
+
+        public IEnumerable<Car> GetAll()
+        {
+            return _db.Cars;
+        }
+
+
+        public Car GetCarByChassis(string chassisSeries)
+        {
+            return _db.Cars.Where(x => x.ChassisSeries == chassisSeries).FirstOrDefault();
+            //return _db.Cars.Where(x => x.ChassisSeries == chassisSeries).Include(c => c.User).FirstOrDefault();
+        }
+
+        public void Remove(int id)
+        {
+            _db.Cars.Remove(this.Get(id));
+        }
+    }
+}
