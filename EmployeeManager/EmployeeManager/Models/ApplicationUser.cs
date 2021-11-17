27f9eb1665f9d8709702578c9b67235c14e37fa7
red-victor <77648244+red-victor@users.noreply.github.com>
2021-11-17 18:39:10 +0200

    Add Columns Types to User and Car Models

--- EmployeeManager/EmployeeManager/Models/ApplicationUser.cs
+++ EmployeeManager/EmployeeManager/Models/ApplicationUser.cs
@@ -1,17 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EmployeeManager.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Column(TypeName = "varchar(30)")]
         public string FirstName { get; set; }
+        [Column(TypeName = "varchar(30)")]
         public string LastName { get; set; }
+        [Column(TypeName = "char(13)")]
         public string CNP { get; set; }
+        [Column(TypeName = "varchar(100)")]
         public string Adress { get; set; }
-        public List<Car> Cars { get; set; } = new List<Car>();
+        public List<Car> Cars { get; set; }
     }
 }
--- EmployeeManager/EmployeeManager/Models/Car.cs
+++ EmployeeManager/EmployeeManager/Models/Car.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,16 +10,20 @@ namespace EmployeeManager.Models
     public class Car
     {
         [Key]
-        public int Id{ get; set; }
+        public int Id { get; set; }
         [Required]
+        [Column(TypeName = "char(17)")]
         public string ChassisSeries { get; set; }
         [Required]
+        [Column(TypeName = "varchar(30)")]
         public string Brand { get; set; }
         [Required]
+        [Column(TypeName = "varchar(30)")]
         public string Model { get; set; }
         [Required]
         public DateTime FirstRegistrationDate { get; set; }
         [Required]
+        [Column(TypeName = "varchar(30)")]
         public string Color { get; set; }
         [Required]
         public int Mileage { get; set; }
