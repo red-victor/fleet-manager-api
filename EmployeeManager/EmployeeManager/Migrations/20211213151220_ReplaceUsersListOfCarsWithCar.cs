889cd4bedc4cfa83eabfbdfc97080934302c79e6
red-victor <77648244+red-victor@users.noreply.github.com>
2021-12-13 17:36:50 +0200

    Replace List of Car with Car in ApplicationUser and add migration

---
+++ EmployeeManager/EmployeeManager/Migrations/20211213151220_ReplaceUsersListOfCarsWithCar.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManager.Migrations
+{
+    public partial class ReplaceUsersListOfCarsWithCar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_UserId",
+                table: "Cars");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "692c33ee-bb30-47a2-9a36-7a44085590a6");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ff47d1a2-4c07-422d-9ee1-e95357837eb1");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "81cf58fb-8d12-48dc-b678-be020bcdd5eb", "bb7c112c-7300-424b-8a52-6b9377d8fcc1", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "4af314f7-01ae-482e-a6e6-16887bb33147", "70372fd7-2766-4373-991a-ce5a0e537125", "Employee", "EMPLOYEE" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_UserId",
+                table: "Cars",
+                column: "UserId",
+                unique: true,
+                filter: "[UserId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_UserId",
+                table: "Cars");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4af314f7-01ae-482e-a6e6-16887bb33147");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "81cf58fb-8d12-48dc-b678-be020bcdd5eb");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "ff47d1a2-4c07-422d-9ee1-e95357837eb1", "0f1d2bd4-d62c-499f-800c-8c2c7e4164a8", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "692c33ee-bb30-47a2-9a36-7a44085590a6", "e412d8b0-1b34-488f-9cb3-9c20a595aaf4", "Employee", "EMPLOYEE" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_UserId",
+                table: "Cars",
+                column: "UserId");
+        }
+    }
+}
