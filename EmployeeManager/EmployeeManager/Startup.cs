bd8511df7b8e2c3f36f6f188d66852ce73e12a2a
red-victor <77648244+red-victor@users.noreply.github.com>
2022-01-10 14:57:26 +0200

    Add Exception Middleware and AppException model

--- EmployeeManager/EmployeeManager/Startup.cs
+++ EmployeeManager/EmployeeManager/Startup.cs
@@ -15,6 +15,7 @@ using System;
 using System.Text;
 using System.IO;
 using System.Reflection;
+using EmployeeManager.Middleware;
 
 namespace EmployeeManager
 {
@@ -89,9 +90,11 @@ namespace EmployeeManager
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                //app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EmployeeManager v1"));
             }
