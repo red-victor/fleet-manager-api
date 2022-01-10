981328c6a831966a9bce18efd9c3bfb54e195af4
red-victor <77648244+red-victor@users.noreply.github.com>
2022-01-10 20:23:19 +0200

    Add Project Seed

---
+++ EmployeeManager/EmployeeManager/Data/Seed.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeManager.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Data
+{
+    public class Seed
+    {
+        public static async Task SeedData(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            if (!context.Cars.Any())
+            {
+                var carList = new List<Car>();
+
+                using (var stream = new MemoryStream())
+                {
+                    var filePath = Path.GetPathRoot(@"\Seed\");
+                    //await file.CopyToAsync(stream);
+                    //carList = Utils.ParseCarsExcel(stream);
+                }
+            }
+        }
+    }
+}
