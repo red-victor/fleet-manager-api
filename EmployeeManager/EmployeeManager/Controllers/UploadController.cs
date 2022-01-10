65a892db6a61dc2b073d9f51f6a3cf856821cf99
red-victor <77648244+red-victor@users.noreply.github.com>
2022-01-10 20:23:06 +0200

    Extract File parser from controller into Utils

--- EmployeeManager/EmployeeManager/Controllers/UploadController.cs
+++ EmployeeManager/EmployeeManager/Controllers/UploadController.cs
@@ -15,32 +15,15 @@ namespace EmployeeManager.Controllers
         [Route("/upload/cars/list")]
         public async Task<IActionResult> UploadCarsExcel(IFormFile file)
         {
-            var list = new List<Car>();
+            var carList = new List<Car>();
+
             using (var stream = new MemoryStream())
             {
                 await file.CopyToAsync(stream);
-
-                using (var package = new ExcelPackage(stream))
-                {
-                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
-                    var rowCount = worksheet.Dimension.Rows;
-                    for (int row = 2; row <= rowCount; row++)
-                    {
-                        list.Add(new Car
-                        {
-                            LicencePlate = worksheet.Cells[row, 1].Value.ToString().Trim(),
-                            ChassisSeries = worksheet.Cells[row, 2].Value.ToString().Trim(),
-                            Brand = worksheet.Cells[row, 3].Value.ToString().Trim(),
-                            Model = worksheet.Cells[row, 4].Value.ToString().Trim(),
-                            //FirstRegistrationDate = worksheet.Cells[row, 5].Value.ToString().Trim(),
-                            Color = worksheet.Cells[row, 6].Value.ToString().Trim(),
-                            //Mileage = int.TryParse(worksheet.Cells[row, 7].Value.ToString().Trim())
-                        });
-                    }
-                }
+                carList = Utils.ParseCarsExcel(stream);
             }
 
-            return Ok(list);
+            return Ok(carList);
         }
     }
 }
--- EmployeeManager/EmployeeManager/Utils.cs
+++ EmployeeManager/EmployeeManager/Utils.cs
@@ -1,6 +1,9 @@
 ﻿using EmployeeManager.Models;
+using Microsoft.AspNetCore.Http;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -32,6 +35,33 @@ namespace EmployeeManager
             }
 
             return values;
+        }
+
+        public static List<Car> ParseCarsExcel(MemoryStream stream)
+        {
+            var list = new List<Car>();
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (var package = new ExcelPackage(stream))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                var rowCount = worksheet.Dimension.Rows;
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    list.Add(new Car
+                    {
+                        LicencePlate = worksheet.Cells[row, 1].Value.ToString().Trim(),
+                        ChassisSeries = worksheet.Cells[row, 2].Value.ToString().Trim(),
+                        Brand = worksheet.Cells[row, 3].Value.ToString().Trim(),
+                        Model = worksheet.Cells[row, 4].Value.ToString().Trim(),
+                        //FirstRegistrationDate = worksheet.Cells[row, 5].Value.ToString().Trim(),
+                        Color = worksheet.Cells[row, 6].Value.ToString().Trim(),
+                        //Mileage = int.TryParse(worksheet.Cells[row, 7].Value.ToString().Trim())
+                    });
+                }
+            }
+
+            return list;
         }
     }
 }
