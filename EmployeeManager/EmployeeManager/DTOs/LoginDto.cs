71cf91c0f4d379e44f597b47e378a61c548b54a7
red-victor <77648244+red-victor@users.noreply.github.com>
2022-01-13 10:26:11 +0200

    Remove Credential Dto, Add Login and Register Dto, Add Phone Number to UserDto

--- EmployeeManager/EmployeeManager/DTOs/LoginDto.cs
+++ EmployeeManager/EmployeeManager/DTOs/LoginDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace EmployeeManager.DTOs
 {
-    public class CredentialDto
+    public class LoginDto
     {
         public string Email { get; set; }
         public string Password { get; set; }
