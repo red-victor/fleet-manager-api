7fc660287ccf3a77cd6d986bbcc6aff61d9c8be9
AdiDD <adi.deaconu21@gmail.com>
2022-01-18 09:00:37 +0200

    Create ChangePasswordDto

---
+++ EmployeeManager/EmployeeManager/DTOs/ChangePasswordDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManager.DTOs
+{
+    public class ChangePasswordDto
+    {
+        [Required]
+        public string CurrentPassword { get; set; }
+        [Required]
+        public string NewPassword { get; set; }
+        [Required]
+        public string ConfirmPassword { get; set; }
+    }
+}
