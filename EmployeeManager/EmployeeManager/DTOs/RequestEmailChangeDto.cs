3475209ec66a0d6aa1dc3d60b8d5892660985835
AdiDD <adi.deaconu21@gmail.com>
2022-01-22 07:46:28 +0200

    Add RequestEmailChangeDto

---
+++ EmployeeManager/EmployeeManager/DTOs/RequestEmailChangeDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.DTOs
+{
+    public class RequestEmailChangeDto
+    {
+        public string UserId { get; set; }
+        public string newEmail { get; set; }
+        public string password { get; set; }
+    }
+}
